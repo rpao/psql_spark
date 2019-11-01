0d03fea1ae2b9588cdc18c8a265c35205fbcd664
Rebeca Oliveira <rpao@cin.ufpe.br>
2019-11-01 07:00:13 -0300

    Calculo de duração, média e desvio padrão do tempo de iteração

--- relatorio.py
+++ relatorio.py
@@ -1,34 +1,106 @@
 import datetime
+import statistics
 
 if __name__ == '__main__':
-    arquivo = 'relatorios/spark/sf10/monitoring_q3'
-    arquivo2 = 'relatorios/spark/sf10/monitoring_f_q3'
-    query = ['1','2','3','4']
 
-    for q in query:
-        origin = open(arquivo + q + '.csv', 'r')
-        ## retirar o cabeçalho
-        origin.readline()
+    fws = ['psql', 'spark']
 
-        dados = []
-        dadosFormatados = []   
+    sfs = ['sf1', 'sf10']
+    
+    queries = ['q11','q12','q13',
+                'q21','q22','q23',
+                'q31','q32','q33','q34',
+                'q41','q42','q43',]
 
-        for line in origin.readlines():
-            dados.append(line.replace('\n','').split(','))
-        origin.close()
+    for fw in fws:
+        for sf in sfs:                
+            for q in queries:
+                if (fw == fws[1]) :
+                    tempOrigem = 'relatorios/' + fw + '/' + sf + '/' + q + '_tempView.csv'
+                    loadRelatorio = 'relatorios/dados/' + fw + '_' + sf + '/LoadTable.csv'
+                    createRelatorio = 'relatorios/dados/' + fw + '_' + sf + '/CreateTmpView.csv'
+                    
+                    origin = open(tempOrigem, 'r')
+                    ## retirar o cabeçalho
+                    origin.readline()
 
-        tempo = 0
-        formatDate = '%Y-%m-%d %H:%M:%S.%f'
-        dtAnterior = datetime.datetime.strptime(dados[0][0], formatDate)
-        dadosFormatados.append('microseconds,cpuPercent,usedMB,memPercent\n')
-        for i in range(len(dados)):
-            ## intervalo de tempo
-            difTempo = dtAnterior - datetime.datetime.strptime(dados[i][0], formatDate)
-            tempo += difTempo.microseconds
-            dtAnterior = datetime.datetime.strptime(dados[i][0], formatDate)
-            dadosFormatados.append(str(tempo)+','+dados[i][1]+','+dados[i][2]+','+dados[i][3]+'\n')
-        
-        formated = open(arquivo2 + q + '.csv','w')
-        formated.writelines(dadosFormatados)
+                    dados = []                
+                    for line in origin.readlines():
+                        dados.append(line.replace(', ',',').replace('\n','').split(','))
+                    origin.close()
 
-        print("ended ...")
+                    micro = 1000000
+                    formatDate = '%Y-%m-%d %H:%M:%S.%f'
+
+                    loadFile = open(loadRelatorio,'a')
+                    createFile = open(createRelatorio,'a')
+
+                    for i in range(len(dados)):
+                        inicio =  datetime.datetime.strptime(dados[i][1], formatDate) 
+                        fim = datetime.datetime.strptime(dados[i][2], formatDate)
+                        duracao = datetime.datetime.__sub__(fim, inicio)
+                        if (dados[i][0] == 'CREATE_VIEW'):
+                            createFile.write(q + ',' + str(duracao.seconds + duracao.microseconds/micro)+'\n')
+                        elif (dados[i][0] == 'LOAD'):
+                            loadFile.write(q + ',' + str(duracao.seconds + duracao.microseconds/micro)+'\n')
+                        
+                    loadFile.close()
+                    createFile.close()
+
+                origem = 'relatorios/' + fw + '/' + sf + '/' + q + '_iteracoes.csv'
+                relatorio = 'relatorios/dados/' + fw + '_' + sf + '/' + q + '.csv'
+
+                print (origem)
+
+                origin = open(origem, 'r')
+                ## retirar o cabeçalho
+                origin.readline()
+
+                dados = []                
+                for line in origin.readlines():
+                    dados.append(line.replace(', ',',').replace('\n','').split(','))
+                origin.close()
+
+                micro = 1000000
+                formatDate = '%Y-%m-%d %H:%M:%S.%f'
+
+                tempo = []
+                for i in range(len(dados)):
+                    inicio =  datetime.datetime.strptime(dados[i][1], formatDate) 
+                    fim = datetime.datetime.strptime(dados[i][2], formatDate)
+                    duracao = datetime.datetime.__sub__(fim, inicio)
+                    tempo.append(duracao.seconds + duracao.microseconds/micro)
+                
+                media = statistics.mean(tempo)
+                desvioPadrao = statistics.stdev(tempo) 
+
+                formated = open(relatorio,'w')
+                formated.write("DuracaoIteracao\n")
+                for t in tempo:
+                    formated.write(str(t) + '\n')
+                formated.write('\nMEDIA: ' + str(media) + '\nDESVIO_PADRAO: ' + str(desvioPadrao))
+                formated.close()
+
+    for sf in sfs:
+        arquivos = ['relatorios/dados/spark_' + sf + '/LoadTable.csv',
+                    'relatorios/dados/spark_' + sf + '/CreateTmpView.csv']
+        for loadRelatorio in arquivos:
+            loadFile = open(loadRelatorio, 'r')
+            loadDados = []                
+            for line in loadFile.readlines():
+                try:
+                    t = line.replace(', ',',').replace('\n','').split(',')[1]
+                    print (t)
+                    loadDados.append(float(t))
+                except:
+                    print(line)
+            loadFile.close()
+            
+            media = statistics.mean(loadDados)
+            desvioPadrao = statistics.stdev(loadDados) 
+
+            loadFile = open(loadRelatorio,'a')
+            loadFile.write('MEDIA: ' + str(media) + '\nDESVIO PADRAO: ' + str(desvioPadrao))
+            loadFile.close()
+
+    print("ended ...")
